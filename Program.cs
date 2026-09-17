926c3b06c1be20d878345844fbe9b40e549ff3bc
Naika Jith <naika.jith@outlook.com>
2026-09-17 12:06:37 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Numbers_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+
+        }
+    }
+}
